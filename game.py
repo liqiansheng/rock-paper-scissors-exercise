2d1f26f9f89ff8ecf79b54d88b809377c0602d12
liqiansheng <87025707+liqiansheng@users.noreply.github.com>
2021-07-14 19:47:14 +0800

    Checking player answer

--- game.py
+++ game.py
@@ -4,7 +4,13 @@ print("Rock, Paper, Scissors, Shoot!")
 playerchoice = input("Please choose either 'rock', 'paper' ,or 'scissors':")
 
 # VALIDATE USER INPUT
-print("You chose:" + playerchoice)
+if playerchoice == 'rock' or playerchoice == 'paper' or playerchoice =='scissors':
+    print("You chose:" + playerchoice)
+else:
+    print("OOPS, INVALID, PLEASE TRY AGAIN")
+    exit()
+
+
 
 # GENERATE A COMPUTER CHOICE
 
