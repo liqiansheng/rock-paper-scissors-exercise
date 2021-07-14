8002f9d876b3188f88ebdc48042de116ca41259d
liqiansheng <87025707+liqiansheng@users.noreply.github.com>
2021-07-15 00:22:51 +0800

    Determine the winner

--- game.py
+++ game.py
@@ -15,10 +15,26 @@ else:
 
 # GENERATE A COMPUTER CHOICE
 
-computerchoice = ["rock","paper","scissors"]
-print("The computer chose: " + random.choice(computerchoice))
-
-
+choicelist = ["rock","paper","scissors"]
+computerchoice = random.choice(choicelist)
+print("The computer chose: " + computerchoice)
 
 # DETERMINE THE WINNER
 
+if playerchoice == "rock" and computerchoice == "paper":
+    print("You lose!")
+elif playerchoice == "paper" and computerchoice == "scissors":
+    print("You lose!")
+elif playerchoice == "scissors" and computerchoice == "rock":
+    print("You lose!")
+elif playerchoice == "rock" and computerchoice == "scissors":
+    print("You win!")
+elif playerchoice == "paper" and computerchoice == "rock":
+    print("You win!")
+elif playerchoice == "scissors" and computerchoice == "paper":
+    print("You win!")
+else:
+    print("Tie!")
+
+    
+    
