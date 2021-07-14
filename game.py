9067b51dd0fce4e68a57ee6db7101392dc10fbed
liqiansheng <87025707+liqiansheng@users.noreply.github.com>
2021-07-14 19:20:38 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
 print("Rock, Paper, Scissors, Shoot!")
+
+# ASK FOR A USER INPUT
+playerchoice = input("Please choose either 'rock', 'paper' ,or 'scissors':")
+
+# VALIDATE USER INPUT
+print("You chose:" + playerchoice)
+
+# GENERATE A COMPUTER CHOICE
+
+# DETERMINE THE WINNER
+
